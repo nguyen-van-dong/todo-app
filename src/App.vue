40366a622bd5a3fa909c4f0c9d7c85d98f2509ba
Dong Nguyen <dongbuh@gmail.com>
2021-03-25 09:32:12 +0700

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+// import Todo from './components/Todo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Avenir, Arial, Helvetica, sans-serif;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+      To do App
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px
+}
+</style>
